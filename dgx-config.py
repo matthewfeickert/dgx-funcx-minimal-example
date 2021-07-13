d0e296b78502a092df227915350560f8d5d9fc7a
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-13 12:11:33 -0500

    Use new service_address

--- dgx-config.py
+++ dgx-config.py
@@ -27,7 +27,7 @@ config = Config(
             ),
         )
     ],
-    funcx_service_address="https://api2.funcx.org/v2",
+    funcx_service_address="https://api.dev.funcx.org/v2"
 )
 
 # For now, visible_to must be a list of URNs for globus auth users or groups, e.g.:
